d58c04ee4bfadcbb4e99b539f848c41e7d515b03
rioverdesho-eng <rioverde.sho@gmail.com>
2026-07-08 10:25:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ const seats = [
 ];
 
 console.log(seats);
+const PASSWORD = "123456";
+
+function login() {
+  const pin = document.getElementById("pin").value;
+
+  if (pin === PASSWORD) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("staff").style.display = "block";
+  } else {
+    alert("暗証番号が違います");
+  }
+}
+
+function startSeat(no) {
+  alert(no + "番打席を利用開始");
+}
+
+function endSeat(no) {
+  alert(no + "番打席を利用終了");
+}
