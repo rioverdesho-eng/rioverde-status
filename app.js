c4dd2525ae1a6fd5e02d4930a1d3fbe799c06858
rioverdesho-eng <rioverde.sho@gmail.com>
2026-07-08 10:57:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-const seats = [
-    "使用中",
-    "空席",
-    "使用中",
-    "空席",
-    "空席",
-    "使用中"
-];
+import { db } from "./firebase.js";
+import {
+  ref,
+  set,
+  onValue
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
 
-console.log(seats);
-const PASSWORD = "123456";
-
-function login() {
-  const pin = document.getElementById("pin").value;
-
-  if (pin === PASSWORD) {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("staff").style.display = "block";
-  } else {
-    alert("暗証番号が違います");
-  }
-}
-
-function startSeat(no) {
-  alert(no + "番打席を利用開始");
-}
-
-function endSeat(no) {
-  alert(no + "番打席を利用終了");
-}
+console.log("Firebase接続成功");
